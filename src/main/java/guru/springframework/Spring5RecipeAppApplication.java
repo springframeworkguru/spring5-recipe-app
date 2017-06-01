11194fbcb1cd9a88041df4944ee61763c53b9443
John Thompson <john@springframework.guru>
2017-06-01 17:39:37 -0400

    init add of project

---
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecipeAppApplication.class, args);
+	}
+}
---
+++ src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5RecipeAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
