813fb4b49bc1e19e24d73e772a6f0014a58220f0
John Thompson <john@springframework.guru>
2017-06-13 15:46:02 -0400

    completing assignment

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -15,7 +15,8 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //private UnitOfMeasure uom;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -50,5 +51,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
