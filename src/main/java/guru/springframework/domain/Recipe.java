6db89d4bc9c28ce0169dc82ad93b48af05e4899f
Bahmed Abdelmajid <bahmedabdelmajid@gmail.com>
2018-01-10 17:00:15 +0000

    Finishing JPA enities

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -4,10 +4,15 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -30,9 +35,21 @@ public class Recipe {
 	
 	@OneToOne(cascade=CascadeType.ALL)
 	private Note notes;
+
+	
+	@Enumerated(value=EnumType.STRING)
+	private Difficulty difficulty;
 	
 	@OneToOne(cascade=CascadeType.ALL,mappedBy="recipe")
 	private Set<Ingredient> ingredients;
+	
+	
+	@ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
 
 	public Long getId() {
 		return id;
