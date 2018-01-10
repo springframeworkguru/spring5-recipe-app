899da2e59b51192dc155d2ba24bab69746414363
Bahmed Abdelmajid <bahmedabdelmajid@gmail.com>
2018-01-10 17:14:03 +0000

    creation and finishing JPA repositories

--- src/main/java/guru/springframework/domain/UnitMease.java
+++ src/main/java/guru/springframework/domain/UnitMease.java
@@ -4,8 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.OneToOne;
 
 @Entity
 public class UnitMease {
