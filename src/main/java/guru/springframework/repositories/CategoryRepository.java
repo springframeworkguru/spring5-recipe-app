5c839763423e8eca26b94476ec0a2613c9e9d083
Piotr Sekula <piotr92345@o2.pl>
2018-01-11 19:23:40 +0100

    Add repositories for category, recipe and unit of measure

---
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.repositories;
+
+import guru.springframework.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+}
