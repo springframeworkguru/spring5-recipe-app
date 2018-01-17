943915b0a94765efc7fd1074b4879e259b83eb85
piotr921 <piotr92345@o2.pl>
2018-01-17 14:28:43 +0100

    Learn add jpa (#1)
    * Add domain classes for recipe, ingredient and notes.
    
    * Add UnitOfMeasure class.
    
    * Add enumerated value for recipe difficulty.
    
    * Add many to many relation and category class.
    
    * Add many to many relationship
    
    * Add repositories for category, recipe and unit of measure
    
    * Add script for populating data
    
    * Add repositories with methods for reading data by id
    
    * Add bootstrap class and service for displaying list of recipes
    
    * Clean code.

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,84 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+        this.recipe = recipe;
+    }
+
+    // region getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
+    // endregion
+}
