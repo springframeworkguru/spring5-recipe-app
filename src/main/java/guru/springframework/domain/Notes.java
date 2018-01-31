39b2079d19b818579dbe04cd1f79d855376c2d2a
Michael Kalisvaart <mkalisvaart@bol.com>
2018-01-31 09:04:07 +0100

    one-to-one-jpa Entities

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -1,43 +1,45 @@
 package guru.springframework.domain;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
 
-/**
- * Created by jt on 6/13/17.
- */
+import static javax.persistence.GenerationType.IDENTITY;
+
 @Entity
 public class Notes {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
-
+    
     @OneToOne
     private Recipe recipe;
-
+    
     @Lob
     private String recipeNotes;
-
+    
     public Long getId() {
         return id;
     }
-
+    
     public void setId(Long id) {
         this.id = id;
     }
-
+    
     public Recipe getRecipe() {
         return recipe;
     }
-
+    
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
     }
-
+    
     public String getRecipeNotes() {
+    
         return recipeNotes;
     }
-
     public void setRecipeNotes(String recipeNotes) {
         this.recipeNotes = recipeNotes;
     }
