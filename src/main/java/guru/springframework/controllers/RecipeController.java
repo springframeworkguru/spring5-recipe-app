71b447dbfa6795aba89b9e8bdf4b66f05a127c55
Ximanta Sarma <simanta.sarma@stackroute.in>
2018-03-20 21:22:52 +0530

    Updated to Spring Boot 2.0.0.RELEASE. Changed New Recipe creation HTTP Method to POST.

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -36,8 +36,7 @@ public class RecipeController {
         return "recipe/recipeform";
     }
 
-    @PostMapping
-    @RequestMapping("recipe")
+    @PostMapping("recipe")
     public String saveOrUpdate(@ModelAttribute RecipeCommand command){
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
