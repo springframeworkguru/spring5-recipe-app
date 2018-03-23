55a8ef42b09d69289c1b1d2d570f51f25941a285
alaa7696 <arsalane.alaa@gmail.com>
2018-03-23 16:08:03 +0000

    adding Unit Of measure and ingredients

---
+++ src/main/java/guru/springframework/model/Ingredient.java
@@ -0,0 +1,60 @@
+package guru.springframework.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String description;
+    private BigDecimal ammount;
+
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private Recipe recipe;
+
+    @OneToOne
+    private UnitOfMeasure unitOfMeasure;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmmount() {
+        return ammount;
+    }
+
+    public void setAmmount(BigDecimal ammount) {
+        this.ammount = ammount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+}
