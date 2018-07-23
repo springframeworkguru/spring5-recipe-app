5eee54443528f87bbed50713876697a7f02a2d98
snallaballi <snallaballi@gainsight.com>
2018-07-23 11:35:36 +0530

    Added a mapping between Recipe and Notes Entity, to maintain a single foreign key.

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -17,7 +17,7 @@ public class Notes {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
+    @OneToOne(mappedBy = "notes")
     private Recipe recipe;
 
     @Lob
