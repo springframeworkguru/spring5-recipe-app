4d070215826d29d062a63002c49e5d8f4a987f27
Amin Abromand <amin.abromand@metaproc.com>
2018-08-23 12:22:31 +0200

    seconde lesson OneToOne

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -11,7 +11,8 @@ public class Ingredient{
 	private String description;
 	private BigDecimal amount;
 
-	//private UnitOfMeasure uom;
+	@ManyToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure uom;
 
 	@ManyToOne
 	private Recipe recipe;
@@ -38,5 +39,13 @@ public class Ingredient{
 
 	public void setRecipe( Recipe recipe ){
 		this.recipe = recipe;
+	}
+
+	public UnitOfMeasure getUom(){
+		return uom;
+	}
+
+	public void setUom( UnitOfMeasure uom ){
+		this.uom = uom;
 	}
 }
