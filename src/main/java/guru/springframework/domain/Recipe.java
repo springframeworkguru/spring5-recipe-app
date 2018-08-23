c0c99deb94839a783cf88e7aa97d7766e4859952
Amin Abromand <amin.abromand@metaproc.com>
2018-08-23 15:18:46 +0200

    created enum class for unit of measure

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -17,8 +17,9 @@ public class Recipe{
 	private String source;
 	private String url;
 	private String directions;
-	//todo add
-	//private Difficulty difficulty;
+
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredients;
@@ -115,5 +116,13 @@ public class Recipe{
 
 	public void setIngredients( Set<Ingredient> ingredients ){
 		this.ingredients = ingredients;
+	}
+
+	public Difficulty getDifficulty(){
+		return difficulty;
+	}
+
+	public void setDifficulty( Difficulty difficulty ){
+		this.difficulty = difficulty;
 	}
 }
