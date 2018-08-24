0f6414a4d13bc8c4fa02a2a27d65ed53ef73d19b
Amin Abromand <amin.abromand@metaproc.com>
2018-08-24 12:14:50 +0200

    test

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -24,7 +24,7 @@ public class Recipe{
 	@Enumerated(value = EnumType.STRING)
 	private Difficulty difficulty;
 
-	@OneToMany(cascade = CascadeType.MERGE, mappedBy = "recipe")
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredients = new HashSet<>();
 
 	@Lob
