7e7c724b2a78cab8faceedf9dca9cbe2ce3a326f
amin <amin.r.abromand@gmail.com>
2018-10-06 14:40:46 +0200

    added ingredient save and delete functions

--- src/main/java/guru/springframework/services/IngredientService.java
+++ src/main/java/guru/springframework/services/IngredientService.java
@@ -6,4 +6,5 @@ public interface IngredientService {
 
 	IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
 	IngredientCommand saveIngredientCommand(IngredientCommand command);
+	void deleteById(Long recipeId, Long idToDelete);
 }
