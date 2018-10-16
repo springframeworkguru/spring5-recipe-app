2dbeb6bb2f8a45a2b42b3835773158874569a1c7
Priyank Singh <priyanksingh8@gmail.com>
2018-10-16 16:05:08 +0530

    Update Spring5RecipeAppApplication.java

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -3,6 +3,7 @@ package guru.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//comment
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
 
