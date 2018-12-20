c68d3e7820eaefa96497c8ca51a266cd18c665a7
Tangi Vass <tangi.vass.ep@biznet.io>
2018-12-20 15:09:39 +0100

    Write Test for IndexController

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,40 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        String viewName = indexController.getIndexPage(model);
+        String attributeName = "recipes";
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes(); // verify that getRecipes is called only one time
+        verify(model, times(1)).addAttribute(eq(attributeName), anySet()); // verify that addAttribute is called only one time, with the proper arguments
+    }
+}
