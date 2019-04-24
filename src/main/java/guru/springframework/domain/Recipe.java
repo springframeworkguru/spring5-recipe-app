69c57b2fc6334aa93688392a1943cb8803568577
DB Patil <dbprojects@gmail.com>
2019-04-24 19:45:42 -0400

    Setup Recipe and Notes

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,97 @@
+/**
+ * 
+ */
+package guru.springframework.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+/**
+ * @author wcsadmin
+ *
+ */
+
+@Entity
+public class Recipe {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String description;
+	private String prepTime;
+	private String cookTime;
+	private String servings;
+	private String url;
+	private String directions;
+	//private Difficulty difficulty;
+	
+	@Lob
+	private Byte[] image;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Notes notes;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPrepTime() {
+		return prepTime;
+	}
+	public void setPrepTime(String prepTime) {
+		this.prepTime = prepTime;
+	}
+	public String getCookTime() {
+		return cookTime;
+	}
+	public void setCookTime(String cookTime) {
+		this.cookTime = cookTime;
+	}
+	public String getServings() {
+		return servings;
+	}
+	public void setServings(String servings) {
+		this.servings = servings;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getDirections() {
+		return directions;
+	}
+	public void setDirections(String directions) {
+		this.directions = directions;
+	}
+	public Byte[] getImage() {
+		return image;
+	}
+	public void setImage(Byte[] image) {
+		this.image = image;
+	}
+	public Notes getNotes() {
+		return notes;
+	}
+	public void setNotes(Notes notes) {
+		this.notes = notes;
+	}
+	
+	
+}
