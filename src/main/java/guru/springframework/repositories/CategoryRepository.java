4d0da211f9fe37b5749f2de13c5441c68bf77a72
Alex Hawley <asimo124@yahoo.com>
2019-05-02 23:44:06 -0500

    Added findByDescription query method for both Category and UnitOfMeasure, which returns optional object. Figured out how to unwrap and account for null unwrapped optionals (Entities not found by id).

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -3,8 +3,13 @@ package guru.springframework.repositories;
 import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
+
 /**
  * Created by jt on 6/13/17.
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findCategoryByDescription(String description);
 }
