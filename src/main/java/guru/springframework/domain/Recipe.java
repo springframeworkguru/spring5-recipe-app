0fa537800c279f4c6f8ae19f8232cc393c9496fe
Alex Hawley <asimo124@yahoo.com>
2019-05-02 21:48:23 -0500

    Setup Many To Many relationship between Recipe Entity and Category Entity. Ran it in h2-console to see that it worked.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -20,8 +20,12 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // TODO - ADD
-    // private Difficulty difficulty;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
@@ -89,6 +93,14 @@ public class Recipe {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 
     public String getDirections() {
