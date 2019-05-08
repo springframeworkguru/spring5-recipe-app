a22188e07e7200e29710c9e0722cdbc01d7dc8fc
Mohamed <mohamed.e@trufla.com>
2019-05-08 13:58:33 +0200

    Added Difficulty

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -16,7 +16,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //private Diffeculty diffeculty;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty diffeculty;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
     @Lob
@@ -110,5 +111,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDiffeculty() {
+        return diffeculty;
+    }
+
+    public void setDiffeculty(Difficulty diffeculty) {
+        this.diffeculty = diffeculty;
     }
 }
