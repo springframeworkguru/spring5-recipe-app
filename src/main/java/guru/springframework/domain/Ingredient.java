db5d424f97ca823ab6af79d0ff0917808e263bf0
Mohamed <mohamed.e@trufla.com>
2019-05-21 16:25:14 +0200

    added Recipe view, failing to fetch ingredients

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -14,15 +14,16 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-    @OneToOne
+    @OneToOne(fetch = FetchType.EAGER)
     private UnitOfMeasure uom;
     @ManyToOne
     private Recipe recipe;
 
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
+        this.recipe = recipe;
 
     }
 
