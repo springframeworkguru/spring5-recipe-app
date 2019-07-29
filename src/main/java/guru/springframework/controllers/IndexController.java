e4917c407cd4fc56af339d993e15ded6a4db39a3
AikoN76 <vincenzo.squillante@gmail.com>
2019-07-29 23:23:18 +0200

    Assignment Lesson 148 - Completed - Lesson: 148.

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -4,7 +4,9 @@ import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -15,25 +17,16 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
+    private final RecipeService recipeService;
 
-    private UnitOfMeasureRepository unitOfMeasureRepository;
-
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
 
-        Optional<Category> category = categoryRepository.findByDescription("American");
-
-        Optional<UnitOfMeasure> uom = unitOfMeasureRepository.findByUom("Teaspoon");
-
-        System.out.println("Cat id: " + category.get().getId());
-        System.out.println("Uon id: " + uom.get().getId());
-
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
---
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -0,0 +1,25 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Set<Recipe> getRecipes() {
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+        return recipeSet;
+    }
+}
