9daa25c287b40e01e830f7450cb50b70e210557f
AikoN76 <vincenzo.squillante@gmail.com>
2019-08-12 22:05:57 +0200

    Added a JUnit test. Lesson 179.

---
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -0,0 +1,31 @@
+package guru.springframework.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp(){
+        category = new Category();
+    }
+
+    @Test
+    public void getId() {
+        Long idValue = 4l;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
