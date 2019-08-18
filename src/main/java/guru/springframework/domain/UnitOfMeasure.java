925d9a544b6f2a196065f7afe2d3c4057bdf5e4b
Cesar Nicolas Cabo <cesar.nicolas.cabo@gmail.com>
2019-08-18 00:55:41 -0300

    Added data.sql

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -12,7 +12,17 @@ import javax.persistence.Id;
 public class UnitOfMeasure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getDescription() {
         return description;
--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -1,9 +1,10 @@
 package guru.springframework.repositories;
 
+import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by ccabo 8/17/19
  */
-public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasureRepository, Long> {
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
 }
