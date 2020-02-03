7933e0d0f4522a91346d084b3e30922f7c70362b
Heydar Rzayev <rzaaeeff@gmail.com>
2020-02-03 14:21:32 +0400

    feat: Custom methods in repository

--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -3,10 +3,13 @@ package guru.springframework.repositories;
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Author: hrzayev
  * Date: 03.02.2020
  */
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
