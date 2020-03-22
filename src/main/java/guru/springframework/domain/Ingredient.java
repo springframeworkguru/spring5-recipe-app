228826a85f720a91daac105dbaf705d3bf3ad657
Christian Preß <Christian.Press@syracos.com>
2020-03-22 19:14:32 +0100

    added domain model classes

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,59 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER) //guess it's normal behaviour but can be kind of a showcase here
+    private UnitOfMeasure unit;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UnitOfMeasure getUnit() {
+        return unit;
+    }
+
+    public void setUnit(UnitOfMeasure unit) {
+        this.unit = unit;
+    }
+}
