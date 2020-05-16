835515e9407373bad408cea6b1505e394e73f36b
dquina <daniel.quina@cetelem.pt>
2020-05-16 19:01:31 +0100

    adding findBy methods

--- src/main/java/guru/springframework/controller/IndexController.java
+++ src/main/java/guru/springframework/controller/IndexController.java
@@ -1,4 +1,34 @@
 package guru.springframework.controller;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
 public class IndexController {
+
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
+	@RequestMapping({"", "/", "/index"})
+	public String getIndexPage() {
+
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("Japanese");
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+		System.out.println("Category id is: " + categoryOptional.get().getId());
+		System.out.println("Unit of measure id is: " + unitOfMeasureOptional.get().getId());
+
+		return "index";
+	}
 }
