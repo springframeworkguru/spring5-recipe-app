11d72545a940b9dcece3a0803f4344d32234845e
Shayan Ahmad <v-shahmad@hotels.com>
2020-05-19 23:58:38 +0200

    Added UnitOfMeasure

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -3,10 +3,12 @@ package guru.springframework.domain;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 /**
  * Ingredient for the Recipe.
@@ -23,6 +25,9 @@ public class Ingredient {
 
     @ManyToOne
     private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
 
     public Long getId() {
         return id;
@@ -54,5 +59,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
     }
 }
