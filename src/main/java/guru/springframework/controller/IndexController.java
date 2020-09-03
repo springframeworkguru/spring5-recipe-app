b3c72a1c09fc8911a14f8c7d8e15d08aa072c1ca
Michał Niemczyk <ext-michal.niemczyk@here.com>
2020-09-03 11:59:38 +0200

    Add repositories for Category and UnitOfMeasure. Add index controller and template.

---
+++ src/main/java/guru/springframework/controller/IndexController.java
@@ -0,0 +1,30 @@
+package guru.springframework.controller;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
+
+@Controller
+public class IndexController {
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @GetMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        System.out.println("categoryOptional.get().getId() = " + categoryOptional.get().getId());
+        System.out.println("unitOfMeasureOptional.get().getId() = " + unitOfMeasureOptional.get().getId());
+        return "index";
+    }
+}
--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package guru.springframework.repositories;
 import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
