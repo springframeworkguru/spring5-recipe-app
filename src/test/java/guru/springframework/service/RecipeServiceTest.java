b394decf227bc9c0961b9309a3b7336e1b3930b5
Michał Niemczyk <ext-michal.niemczyk@here.com>
2020-09-14 16:52:00 +0200

    Add simple unit test for RecipeService

---
+++ src/test/java/guru/springframework/service/RecipeServiceTest.java
@@ -0,0 +1,41 @@
+package guru.springframework.service;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeService(recipeRepository);
+    }
+
+    @Test
+    public void findAll() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.findAll();
+
+        Assertions.assertThat(recipes).hasSize(1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
