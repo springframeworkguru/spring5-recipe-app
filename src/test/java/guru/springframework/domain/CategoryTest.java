4f30e59e9d1d54eebec94545e3f14568e0679bb8
Michał Niemczyk <ext-michal.niemczyk@here.com>
2020-09-14 16:41:33 +0200

    Unit test for category id's

---
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp() throws Exception {
+        category = new Category();
+    }
+
+    @Test
+    public void getId() {
+        Long idValue = 4L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+}
