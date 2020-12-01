be111f6ae214f7bef7c0de8a1a25abdc353d10dd
youssef chafai el alaoui <youssefchafaielalaoui@gmail.com>
2020-12-01 18:09:53 +0100

    test branch

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -16,7 +16,7 @@ public class Ingredient {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String description;
+	private String description;  
 	private BigDecimal amount;
 	
 	@ManyToOne
