32c20bb3e403bc5ff4a45ccf95d4fd9d0e7b6944
youssef chafai el alaoui <youssefchafaielalaoui@gmail.com>
2020-12-01 18:08:27 +0100

    completing the assignement

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -3,10 +3,12 @@ package guru.springframework.domain;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ingredient {
@@ -20,6 +22,9 @@ public class Ingredient {
 	@ManyToOne
 	private Recipe recipe;
 
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure uom;
+	
 	public Long getId() {
 		return id;
 	}
@@ -51,6 +56,15 @@ public class Ingredient {
 	public void setRecipe(Recipe recipe) {
 		this.recipe = recipe;
 	}
+
+	public UnitOfMeasure getUom() {
+		return uom;
+	}
+
+	public void setUom(UnitOfMeasure uom) {
+		this.uom = uom;
+	}
+	
 	
 	
 }
