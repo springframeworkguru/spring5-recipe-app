e8fffb89d45ecde77dc6c1be4a4fb934de967dc1
Marc <marc.marechal@dell.com>
2020-12-22 17:02:43 +0800

    Create repositories.

---
+++ src/main/java/guru/springframework/repository/UnitOfMeasureRepository.java
@@ -0,0 +1,6 @@
+package guru.springframework.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasureRepository, Long> {
+}
