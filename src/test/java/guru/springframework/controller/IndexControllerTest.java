abe6d5c2a7f41ac3e943ddbbedb274464b2b290a
Marc <marc.marechal@dell.com>
2020-12-26 20:20:04 +0800

    Add argument matcher for Index Controller Test.

--- src/test/java/guru/springframework/controller/IndexControllerTest.java
+++ src/test/java/guru/springframework/controller/IndexControllerTest.java
@@ -4,11 +4,13 @@ import guru.springframework.domain.Recipe;
 import guru.springframework.service.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -31,10 +33,30 @@ public class IndexControllerTest {
 
     @Test
     public void getIndex() {
+
+        // given
         String expectedView = "index";
+
+        HashSet<Recipe> recipes = new HashSet<>();
+        Recipe recipe = new Recipe();
+        recipes.add(recipe);
+
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(2L);
+        recipes.add(recipe1);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<HashSet<Recipe>> argumentCaptor = ArgumentCaptor.forClass(HashSet.class);
+
+        // when
         String view = indexController.getIndex(model);
+
+        // then
         assertEquals(expectedView, view);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute("recipes", recipeService.getRecipes());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> actualRecipes = argumentCaptor.getValue();
+        assertEquals(actualRecipes.size(), 2);
     }
 }
