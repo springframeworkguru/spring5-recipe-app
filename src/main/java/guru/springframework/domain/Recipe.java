6b61c973fd08b7fcd359c98a39895aa6b71db39a
Sanjeet Kumar <sanjeet.kumar@smartenspaces.com>
2021-03-01 15:49:25 +0530

    added category Entity class.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -9,7 +9,10 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -26,18 +29,22 @@ public class Recipe {
 	private String source;
 	private String url;
 	private String directions;
-	
+
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipeInIngredientEntity")
 	private Set<Ingredient> ingredients;
-	
+
 	@Lob
 	private Byte[] image;
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
-	
+
 	@Enumerated(value = EnumType.STRING)
 	private Difficulty difficulty;
-	
+
+	@ManyToMany
+	@JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories;
+
 	public Long getId() {
 		return id;
 	}
@@ -116,6 +123,30 @@ public class Recipe {
 
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
 	}
 
 }
