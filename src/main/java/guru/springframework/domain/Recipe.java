4c140672c8c88e69db1c9025606b2228acfeb493
mario.magno <mgmagnoj@gmail.com>
2021-03-02 12:39:50 -0300

    Enum Types

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -33,6 +35,9 @@ public class Recipe {
 
     @Lob
     private Byte[] image;
+    
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -117,6 +122,16 @@ public class Recipe {
     public void setImage(Byte[] image) {
         this.image = image;
     }
+
+
+    public Difficulty getDifficulty() {
+        return this.difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
 
     public Notes getNotes() {
         return this.notes;
