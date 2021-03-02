eb18d118c535d1154b0f273481f60ea58950e944
mario.magno <mgmagnoj@gmail.com>
2021-03-02 13:46:39 -0300

    Working

--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -1,9 +1,13 @@
 package guru.springframework.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import guru.springframework.domain.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional <UnitOfMeasure> findByDescription(String description);
     
 }
