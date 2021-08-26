6015f96d401053aaadf2bb61f9b35b023af5087d
Mirko Leschikar <fafnirx@gmail.com>
2021-08-26 16:17:12 +0200

    Adding repositories and basic data

---
+++ src/main/java/guru/springframework/domain/Category.java
@@ -0,0 +1,40 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
