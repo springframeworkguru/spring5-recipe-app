3457324f20b5575896dd32f6af9599fb5f3b9b57
Hassan Gamal <37403366+hassan1310@users.noreply.github.com>
2022-02-09 14:33:08 +0300

    recipe project - add entities

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -4,18 +4,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
 
 @Entity
-public class Notes {
+public class Category {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@Lob
-	private String  notes;
-	@OneToOne
+	private Long id ;
+	@NotNull
+	private String departmentName;
+	@ManyToMany
 	private Recipe recipe;
+	
 
 }
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,5 +1,118 @@
 package guru.springframework.domain;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Recipe {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Integer prepTime;
+	private Integer cooktime;
+	private Integer savings;
+	private String source;
+	private String url;
+	private String directions;
+	@Lob
+	private byte image[] ;
+	@OneToOne(cascade = CascadeType.ALL)
+	private Notes notes;
+	@ManyToMany(cascade = CascadeType.ALL)
+	private Category category;
+	@OneToMany(cascade = CascadeType.ALL,mappedBy = "recipe")
+	private Set <Ingredient> ingredients;
+	@Enumerated(value=EnumType.STRING)
+	private Difficulty difficulty ;
+	
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+	public Notes getNotes() {
+		return notes;
+	}
+	public void setNotes(Notes notes) {
+		this.notes = notes;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Integer getPrepTime() {
+		return prepTime;
+	}
+	public void setPrepTime(Integer prepTime) {
+		this.prepTime = prepTime;
+	}
+	public Integer getCooktime() {
+		return cooktime;
+	}
+	public void setCooktime(Integer cooktime) {
+		this.cooktime = cooktime;
+	}
+	public Integer getSavings() {
+		return savings;
+	}
+	public void setSavings(Integer savings) {
+		this.savings = savings;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getDirections() {
+		return directions;
+	}
+	public void setDirections(String directions) {
+		this.directions = directions;
+	}
+	public byte[] getImage() {
+		return image;
+	}
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	
+	
+	
+	
 
 }
