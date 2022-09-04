2c1d557b4180a0259f3655e9164b469f0dd3dc53
Martina Baroi <m_baroi@yahoo.com>
2022-09-04 12:38:57 -0400

    added relation

---
+++ src/main/java/guru/springframework/controller/IndexController.java
@@ -0,0 +1,5 @@
+package guru.springframework.controller;
+
+public class IndexController {
+
+}
---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,65 @@
+package guru.springframework.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Ingredient {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+    
+    @ManyToOne			//many ingredients can have in one recipe
+    private Recipe recipe; //this name "recipe" have to write in Recipe entity mappedBy = " "
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+    
+    
+    
+}
