8d6af8458327e88bc9cb75ab374a1efc7c3ecb43
Adinath Jadhav <adinathjadhav@benchmarkit.solutions>
2022-09-16 19:50:54 +0530

    completing assignment

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,11 @@
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class IndexController {
+    @RequestMapping({"", "/","/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
