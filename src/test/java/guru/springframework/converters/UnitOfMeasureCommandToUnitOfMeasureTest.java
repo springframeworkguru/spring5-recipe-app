ee9c532fa306b26c801ed87431aff541539ae7e7
Simanta.Sarma <ximanta.sarma@gmail.com>
2022-10-14 11:27:51 +0530

    Updated to Java 17, Spring Boot 2.7.2, and JUnit 5

--- src/test/java/guru/springframework/converters/UnitOfMeasureCommandToUnitOfMeasureTest.java
+++ src/test/java/guru/springframework/converters/UnitOfMeasureCommandToUnitOfMeasureTest.java
@@ -2,10 +2,10 @@ package guru.springframework.converters;
 
 import guru.springframework.commands.UnitOfMeasureCommand;
 import guru.springframework.domain.UnitOfMeasure;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UnitOfMeasureCommandToUnitOfMeasureTest {
 
@@ -14,7 +14,7 @@ public class UnitOfMeasureCommandToUnitOfMeasureTest {
 
     UnitOfMeasureCommandToUnitOfMeasure converter;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         converter = new UnitOfMeasureCommandToUnitOfMeasure();
 
