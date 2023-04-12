b23a98b9fc67f7fb9f56212c8e8433d2d9463f4e
Nazareno <nazarodriguez56@gmail.com>
2023-04-12 16:34:57 -0300

    Database initialize

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -1,14 +1,13 @@
 package guru.springframework.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 public class Category {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) Values ('American');
+INSERT INTO category (description) Values ('Italian');
+INSERT INTO category (description) Values ('Mexican');
+INSERT INTO category (description) Values ('Fast Food');
+INSERT INTO unit_of_measure (description) Values ('Teaspoon');
+INSERT INTO unit_of_measure (description) Values ('Tablespoon');
+INSERT INTO unit_of_measure (description) Values ('Cup');
+INSERT INTO unit_of_measure (description) Values ('Pinch');
+INSERT INTO unit_of_measure (description) Values ('Ounce');
