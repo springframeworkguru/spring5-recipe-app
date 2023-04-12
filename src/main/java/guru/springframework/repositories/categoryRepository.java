0fb07d1a2062115f3b55c7db680af3fb05e80d7e
Nazareno <nazarodriguez56@gmail.com>
2023-04-12 20:14:25 -0300

    findByDescription for CategoryRepository and UnitOfMeasureRepository with Spring Data JPA Query Methods

--- src/main/java/guru/springframework/repositories/categoryRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.repositories;
-
-import guru.springframework.domain.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface categoryRepository extends CrudRepository<Category, Long> {
-}
