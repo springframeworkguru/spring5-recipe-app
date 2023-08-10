5594c78b0641cf6f7178e340d2e419bb5893eb35
Arman Ebrahimi <armanebrhm@gmail.com>
2023-08-10 21:25:11 +0200

    adding UnitOfMeasure Entity and establishing one to one relationship

--- src/main/java/guru/springframework/model/Ingredient.java
+++ src/main/java/guru/springframework/model/Ingredient.java
@@ -12,6 +12,8 @@ public class Ingredient {
     private BigDecimal amount;
     @ManyToOne
     private Recipe recipe;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
 
     public Long getId() {
         return Id;
---
+++ src/main/java/guru/springframework/model/UnitOfMeasure.java
@@ -0,0 +1,30 @@
+package guru.springframework.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
