75a02a4f17fb1e68b11ba437bc9d88d28f885edd
Ashish <ashishmbm23@gmail.com>
2023-08-30 10:28:11 +0530

    added javax persistence api and updated application to use Junit5.

--- src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
@@ -1,11 +1,12 @@
 package guru.springframework;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class Spring5RecipeAppApplicationTests {
 
