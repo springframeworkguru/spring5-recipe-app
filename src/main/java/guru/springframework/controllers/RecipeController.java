dbc654faf077c790ab498bc2abfee6ce2d560640
Ashish <ashishmbm23@gmail.com>
2023-09-28 12:19:42 +0530

    Creating recipe

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,12 +1,16 @@
 package guru.springframework.controllers;
 
+import guru.springframework.command.RecipeCommand;
+import guru.springframework.domain.Difficulty;
 import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,6 +25,19 @@ public class RecipeController {
         Recipe recipe = recipeService.findById(Long.parseLong(id));
         model.addAttribute("recipe", recipe);
         return "recipe/show";
+    }
+
+    @RequestMapping("/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @RequestMapping("/save")
+    @PostMapping
+    public String saveOrUpdateRecipe(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand savedRecipe = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/"+ savedRecipe.getId() ;
     }
 
 }
