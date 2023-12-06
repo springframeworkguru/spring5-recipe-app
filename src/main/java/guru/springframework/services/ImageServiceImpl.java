bd0974185c472d0f9d6a81de7e067a32685dcb41
bbqboneless <sammylicea5@gmail.com>
2023-12-06 08:58:51 -0600

    Images into database

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -1,14 +1,39 @@
 package guru.springframework.services;
 
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.transaction.Transactional;
+import java.io.IOException;
+
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService{
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file){
-        log.debug("Received a file");
+        try{
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+            int i = 0;
+
+            for(byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+        } catch(IOException e) {
+            log.error("Error Occurred",e);
+            e.printStackTrace();
+        }
     }
 }
