5952d5ac5a064120c210cdbaa60da56f0ab58ad8
Fyodor Losevskiy <losevskiyfz@gmail.com>
2024-01-02 05:21:37 +0300

    Do John Thompson's implementation of assignment

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -11,6 +11,8 @@ import org.springframework.ui.Model;
 import java.util.Set;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -30,12 +32,12 @@ public class IndexControllerTest {
     @Test
     public void getIndexPage() {
 
-        String result = indexController.getIndexPage(model);
+        String viewName = indexController.getIndexPage(model);
 
-        assertEquals(result, "index");
+        assertEquals(viewName, "index");
 
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute("recipes", recipeService.getRecipes());
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
 
     }
 }
