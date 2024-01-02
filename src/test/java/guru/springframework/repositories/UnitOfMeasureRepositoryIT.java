6397e1ec13c4af6d46a7ad18279eb51bb44e59c5
Fyodor Losevskiy <losevskiyfz@gmail.com>
2024-01-02 06:44:36 +0300

    Add Maven Failsafe plugin

--- src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import guru.springframework.domain.UnitOfMeasure;
 
